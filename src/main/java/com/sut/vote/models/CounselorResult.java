814e4518a0355a0d5b6d6dfd3a0ad9667c6b40e1
dell <243322751@qq.com>
2018-12-02 15:04:25 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/resources/mapper/LearningQsMapper.xml

---
+++ src/main/java/com/sut/vote/models/CounselorResult.java
@@ -0,0 +1,31 @@
+package com.sut.vote.models;
+
+public class CounselorResult {
+    private String name;
+    private String avg;
+    private String percent;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvg() {
+        return avg;
+    }
+
+    public void setAvg(String avg) {
+        this.avg = avg;
+    }
+
+    public String getPercent() {
+        return percent;
+    }
+
+    public void setPercent(String percent) {
+        this.percent = percent;
+    }
+}
